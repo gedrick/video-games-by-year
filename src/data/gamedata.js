d1b3e6ba8c9109e11f16dc37af79da4d9d0751d3
Chris <clee@dramafever.com>
2020-01-02 14:25:06 -0500

    Cleanup, fix this

--- src/data/gamedata.js
+++ src/data/gamedata.js
@@ -1,16 +1,16 @@
-import { default as games2007 } from "../data/2007.js";
-import { default as games2008 } from "../data/2008.js";
-import { default as games2009 } from "../data/2009.js";
-import { default as games2010 } from "../data/2010.js";
-import { default as games2011 } from "../data/2011.js";
-import { default as games2012 } from "../data/2012.js";
-import { default as games2013 } from "../data/2013.js";
-import { default as games2014 } from "../data/2014.js";
-import { default as games2015 } from "../data/2015.js";
-import { default as games2016 } from "../data/2016.js";
-import { default as games2017 } from "../data/2017.js";
-import { default as games2018 } from "../data/2018.js";
-import { default as games2019 } from "../data/2019.js";
+import { default as games2007 } from '../data/2007.js';
+import { default as games2008 } from '../data/2008.js';
+import { default as games2009 } from '../data/2009.js';
+import { default as games2010 } from '../data/2010.js';
+import { default as games2011 } from '../data/2011.js';
+import { default as games2012 } from '../data/2012.js';
+import { default as games2013 } from '../data/2013.js';
+import { default as games2014 } from '../data/2014.js';
+import { default as games2015 } from '../data/2015.js';
+import { default as games2016 } from '../data/2016.js';
+import { default as games2017 } from '../data/2017.js';
+import { default as games2018 } from '../data/2018.js';
+import { default as games2019 } from '../data/2019.js';
 
 export default {
   2007: games2007,
