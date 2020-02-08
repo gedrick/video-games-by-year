b9f1c3d5c27f773093226b8402e72c1870b5d0c2
Geddy <g3ddylee@gmail.com>
2020-02-08 10:19:36 -0500

    Delete files that were moved to wikipedia-table-scraper

--- filters/extract-month.js
+++
@@ -1,9 +0,0 @@
-module.exports = function($, obj) {
-  const str = $(obj).text();
-  if (str.includes(',')) {
-    const dayMonth = str.split(',')[0];
-    return dayMonth.split(' ')[0];
-  } else {
-    return null;
-  }
-};
--- filters/trim-text.js
+++
@@ -1,4 +0,0 @@
-module.exports = function trimText($, obj) {
-  const str = $(obj).text();
-  return str.trim();
-};
