5ea0b88db54389a4f445576feb4d44ace08fbda3
Geddy <g3ddylee@gmail.com>
2020-02-11 18:04:35 -0500

    Deleting lots of old scraping folders.

--- scraping/n64/filters/extract-day.js
+++
@@ -1,9 +0,0 @@
-module.exports = function($, obj) {
-  const str = $(obj).text();
-  if (str.includes(',')) {
-    const dayMonth = str.split(',')[0];
-    return parseInt(dayMonth.split(' ')[1].trim());
-  } else {
-    return null;
-  }
-};
